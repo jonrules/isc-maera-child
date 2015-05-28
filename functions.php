e89e747f85595444e32bfbe80a243d7792879542
Jonathan <jonrules@gmail.com>
2015-05-28 13:39:10 -0400

    pre-production updates

--- functions.php
+++ functions.php
@@ -6,4 +6,59 @@ function theme_enqueue_styles() {
 		get_stylesheet_directory_uri() . '/style.css',
 		array( 'parent-style' )
 	);
+	wp_enqueue_script( 'isc-scripts', get_stylesheet_directory_uri() . '/js/isc-scripts.js', array( 'jquery' ) );
 }
+
+function maera_child_mime_types( $mimes ) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter( 'upload_mimes', 'maera_child_mime_types');
+
+function jptweak_remove_share() {
+    remove_filter( 'the_content', 'sharing_display',19 );
+    remove_filter( 'the_excerpt', 'sharing_display',19 );
+    if ( class_exists( 'Jetpack_Likes' ) ) {
+        remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
+    }
+}
+//add_action( 'loop_start', 'jptweak_remove_share' );
+
+function maera_child_jetpack_sharing_buttons( $atts ) {
+	require_once( WP_PLUGIN_DIR . '/jetpack/modules/likes.php' );
+	if ( function_exists( 'sharing_display' ) ) {
+		//sharing_display( '', true );
+	}
+ 
+	if ( class_exists( 'Jetpack_Likes' ) ) {
+		$custom_likes = new Jetpack_Likes;
+		return $custom_likes->post_likes( '' );
+	}
+	return '';
+}
+//add_action( 'theme_mod_header_content', 'maera_child_jetpack_sharing_buttons', 100, 1 );
+//add_shortcode( 'maera_child_jetpack_sharing_buttons', 'maera_child_jetpack_sharing_buttons' );
+
+function maera_child_vc_jetpack_sharing_buttons() {
+	if ( ! function_exists( 'vc_map' ) ) {
+		return;
+	}
+
+	vc_map( array(
+		'name' => __( 'Jetpack Sharing Buttons', 'maera-child' ),
+		'base' => 'maera_child_jetpack_sharing_buttons',
+		'class' => '',
+		'category' => __( 'Content', 'maera-child'),
+		//'icon' => 'icon-wpb-row',
+		//'weight' => 1000,
+		'show_settings_on_create' => false,
+		'params' => array(	
+		)
+	) );	
+}
+//add_action( 'vc_before_init', 'maera_child_vc_jetpack_sharing_buttons' );
+
+function maera_child_header_content( $content ) {
+	return html_entity_decode( $content );
+}
+add_filter( 'theme_mod_header_content', 'maera_child_header_content' );
---
+++ js/isc-scripts.js
@@ -0,0 +1,21 @@
+jQuery(function ($) {
+	var $menu = $('#mobile-menu');
+	var $anchors = $('article.page').find('h1,h2,h3,h4,h5,h6');
+	var $scrollSpyAnchors = $();
+	$anchors.each(function () {
+		var $header = $(this);
+		if (!$header.attr('id')) {
+			return;
+		}
+		$header.addClass('scrollspy');
+		var $menuItem = $('<li class="scroll-spy-menu-item"></li>');
+		var $menuLink = $('<a class="scroll-spy-link waves-effect"></a>')
+			.text($header.text())
+			.attr('href', '#' + $header.attr('id'))
+			.addClass($header[0].tagName.toLowerCase() + '-link');
+		$menuItem.append($menuLink);
+		$menu.append($menuItem);
+		$scrollSpyAnchors = $scrollSpyAnchors.add($header);
+	});
+	$scrollSpyAnchors.scrollSpy();
+});
